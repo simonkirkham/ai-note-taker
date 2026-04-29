8c7ae9cd04a9c36d4789f8be31b23dfbe613f3b4
Simon Kirkham <simon.kirkham@gmail.com>
2026-04-29 15:46:06 +0100

    added second endpoint

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -4,5 +4,6 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 var app = builder.Build();
 
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+app.MapGet("/secret", () => Results.Ok(new { status = "shhhh...." }));
 
 app.Run();
